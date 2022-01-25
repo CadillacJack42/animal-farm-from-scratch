a35425440eac42ab2915733af192a762e8624dfd
James Keicher <jamesdkeicher@gmail.com>
2022-01-25 12:08:55 -0800

    test: fix test to reflect app design

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
       <Main animalsArr={animals} />
       <Footer email={userEmail} />
     </>
-   
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hello and welcome to cadillac jacks animal farm/i);
   expect(linkElement).toBeInTheDocument();
 });
